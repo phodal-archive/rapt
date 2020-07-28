13734c3e6128ee46ed2deed223339c0bed623d40
Phodal Huang <h@phodal.com>
2020-07-28 21:25:56 +0800

    feat: init pxiesl

--- src/crunch/png.rs
+++ src/crunch/png.rs
@@ -2,18 +2,24 @@ use std::path::{PathBuf, Path};
 use image::GenericImageView;
 
 pub struct Png {
-
+    width: u32,
+    height: u32
 }
 
 impl Png {
-    pub fn read(path: PathBuf) {
+    pub fn new(width: u32, height: u32) -> Png {
+        Png { width, height }
+    }
+
+    pub fn read(path: PathBuf) -> Png {
         let mut img = image::open(path).unwrap();
 
-        let img_width = img.dimensions().0;
-        let img_height = img.dimensions().1;
+        let width = img.dimensions().0;
+        let height = img.dimensions().1;
 
         for p in img.pixels() {
             println!("pixel: {:?}", p.2);
         }
+        Png::new(width, height)
     }
 }
