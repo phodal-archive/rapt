9585c25c13c5b76a15382504dca71efc617ee9f9
Phodal Huang <h@phodal.com>
2020-07-30 08:08:24 +0800

    fix: make compile pass

--- src/format/container.rs
+++ src/format/container.rs
@@ -5,16 +5,15 @@ static K_CONTAINER_FORMAT_MAGIC: u32 = 0x54504141;
 static K_CONTAINER_FORMAT_VERSION: u32 = 1;
 static K_PADDING_ALIGNMENT: i32 = 4;
 
-#[derive(Clone)]
 pub struct ContainerWriter<'a> {
     out: CodedOutputStream<'a>,
     entry_count: i32,
 }
 
 impl<'a> ContainerWriter<'a> {
-    pub fn new(out: CodedOutputStream, entry_count: i32) -> ContainerWriter<'a> {
-        ContainerWriter { out, entry_count }
-    }
+    // pub fn new(out: CodedOutputStream, entry_count: i32) -> ContainerWriter<'a> {
+    // ContainerWriter { out, entry_count }
+    // }
 }
 
 #[derive(Clone, Debug)]
