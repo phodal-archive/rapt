b1d1bc4389ad87da20b8af92e0ce580b196c685e
Phodal Huang <h@phodal.com>
2020-07-30 23:27:52 +0800

    feat: make lifecycle works for container

--- src/androidrw/mod.rs
+++ src/androidrw/mod.rs
@@ -3,8 +3,6 @@ extern "C" {}
 
 #[cfg(test)]
 mod tests {
-    use crate::proto::Resources::ResourceTable;
-
     #[test]
     fn should_enable_read_file() {
         println!("cargo:rustc-link-search=/Users/fdhuang/works/android/rapt/src/androidrw/");
--- src/format/container.rs
+++ src/format/container.rs
@@ -7,17 +7,18 @@ static K_CONTAINER_FORMAT_MAGIC: u32 = 0x54504141;
 static K_CONTAINER_FORMAT_VERSION: u32 = 1;
 static K_PADDING_ALIGNMENT: i32 = 4;
 
-pub struct ContainerWriter {
+pub struct ContainerWriter<'b> {
     entry_count: i32,
+    out: CodedOutputStream<'b>,
 }
 
-impl ContainerWriter {
+impl<'b> ContainerWriter<'b> {
     pub fn new<'a>(mut out: CodedOutputStream, entry_count: i32) -> ContainerWriter {
         out.write_raw_little_endian32(K_CONTAINER_FORMAT_MAGIC);
         out.write_raw_little_endian32(K_CONTAINER_FORMAT_VERSION);
         out.write_raw_little_endian32(entry_count.clone() as u32);
 
-        ContainerWriter { entry_count }
+        ContainerWriter { entry_count, out }
     }
 
     pub fn add_res_file_entry(&self, file: CompiledFile, std_in: Stdin) {}
